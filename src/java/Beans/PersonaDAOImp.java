8a9a06bdaa254d2b5b8f94b68af62468f1b3da0d
iLinko <iianriverk@gmail.com>
2024-06-16 19:16:36 -0600

    Preparacion para codificacion de Implementadores de interfazo PersonaDAO

---
+++ src/java/Beans/PersonaDAOImp.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Beans;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.Connection;
+
+public class PersonaDAOImp implements PersonaDAO{
+    
+    private String TABLE_NAME;
+    private ConexionMySQL conMySQL;
+    private Connection conn;
+    
+    public PersonaDAOImp(){
+        TABLE_NAME = "persona";
+        conMySQL = new ConexionMySQL();
+    }
+
+    @Override
+    public void insert(Persona persona) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Persona persona) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Integer id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Persona read(Integer id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Persona> readAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
